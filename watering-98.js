a1960d4f01203462f578edda2d46d5042b3d853a
Zeno Zeng <zenoes@qq.com>
2012-12-15 15:05:00 +0800

    *** empty log message ***

--- watering-98.js
+++ watering-98.js
@@ -31,13 +31,39 @@ var loadscript  = {
 function init() {
     console.log('INIT');
 
-    var useCurrentUser = false;
-    if(!users) {
-        useCurrentUser = true;
-    }
+    $('body').append('<div style="opacity:.8;border-radius:8px;box-shadow: 0 0 15px #000;background:#000;color:#fff;position:fixed;width:600px;height:500px;top:50%;left:50%;margin-left:-300px;margin-top:-250px;z-index:9999;"><div style="padding:3em;">请按照下面示例的格式输入，任意多个都可以，但要遵照格式。<div id="done" style="color:#000;float:right;background:#fff;border-radius:5px;padding:1em;margin-top:-1.5em;cursor:pointer;">DONE</div><br><br><br><input type="checkbox" style="float:left;"/><div style="float:left;">用且仅用当前已登录用户发帖</div><br><br><br>用户及密码（若勾选了上面的，不用管）<br><br><textarea id="users" style="border-radius:5px;padding:1em;outline:none;width:90%;height:5em;">用户一\n用户一密码\n用户二\n用户二密码\n</textarea><br><br><br><textarea id="targets" style="width:90%;outline:none;height:5em;border-radius:5px;padding:1em;">帖子一地址\n帖子二地址</textarea><br><br><br><textarea id="contents" style="width:90%;outline:none;height:5em;border-radius:5px;padding:1em;">随机回复内容一\n随机回复内容二</textarea></div></div>');
+
+    // 若存在localStroage则从中读取，并写入上面的弹窗
+
+
+    $('#done').click(function() {
+        // 解析数据
+
+
+        if(!users) {
+            useCurrentUser = true;
+        }
+
+        // 将这些东西写入localStroage
+        localStorage.setItem('targetPosts', JSON.stringify(targetPosts));
+
+        if(!useCurrentUser)
+          localStorage.setItem('users', JSON.stringify(users));
+
+        start();
+    });
+
+
     console.log('use Current User: '+useCurrentUser);
+}
 
+function start() {
+    $('body').append('<div id="log" style="position:fixed;background:#000;top:0;left:0;width:100%;height:100%;"></div>');
+    log('Init Done.');
+}
 
+function log(content) {
+    $('#log').prepend('QSC-Watering-98.js # '+content+'<br>');
 }
 
 function switchUser() {
@@ -52,27 +78,24 @@ function autoPost() {
 }
 
 
+// 只使用当前已登录的账户？ 若为false，则使用users列表中提供的账户
+var useCurrentUser = true;
+
+// 这里放帖子的地址
+var targetPosts = [];
+
+// var users = [{
+//     username:'username',
+//     password:'password'
+// }];
+
+var users = [];
+
+
+
 // 从localStroage读取users 和 targetPosts
 
-var users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : false;
-var targetPosts = localStorage.getItem('targetPosts') ? JSON.parse(localStorage.getItem('targetPosts')) : false;
-
-
-
-if(!targetPosts) {
-    console.log('!targetPosts');
-    console.log('requre: Watering-98-init.js');
-
-    loadscript.js("http://code.jquery.com/jquery-1.8.3.min.js", function() {
-        console.log('jQuery Done');
-        loadscript.js("https://github.com/zenozeng/qsc-js/raw/master/watering-98-init.js", function () {
-            console.log('watering-98-init.js Done');
-        });
-    });
-
-} else {
-    loadscript.js("http://code.jquery.com/jquery-1.8.3.min.js", function() {
-        console.log('jQuery Done');
-        init();
-    });
-}
+loadscript.js("http://code.jquery.com/jquery-1.8.3.min.js", function() {
+    console.log('jQuery Done');
+    init();
+});
