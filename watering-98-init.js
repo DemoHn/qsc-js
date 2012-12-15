703ff31f7946be4ca206a1a64afa80410e9180e0
Zeno Zeng <zenoes@qq.com>
2012-12-15 11:54:04 +0800

    update ui

--- watering-98-init.js
+++ watering-98-init.js
@@ -11,7 +11,7 @@ var users = [{
 }];
 
 // 弹窗，让用户输入上面的设置
-$('body').append('<div style="opacity:.8;border-radius:8px;box-shadow: 0 0 15px #000;background:#000;color:#fff;position:fixed;width:600px;height:500px;top:50%;left:50%;margin-left:-300px;margin-top:-250px;z-index:9999;"><div style="padding:1em;">请按照下面示例的格式输入，任意多个都可以，但要遵照格式。<textarea id="users">用户一<br>用户一密码<br>用户二<br>用户二密码<br></textarea><textarea id="targets">帖子一地址<br>帖子二地址</textarea></div></div>');
+$('body').append('<div style="opacity:.8;border-radius:8px;box-shadow: 0 0 15px #000;background:#000;color:#fff;position:fixed;width:600px;height:500px;top:50%;left:50%;margin-left:-300px;margin-top:-250px;z-index:9999;"><div style="padding:3em;">请按照下面示例的格式输入，任意多个都可以，但要遵照格式。<div id="done" style="color:#000;float:right;background:#fff;border-radius:5px;padding:1em;margin-top:-1.5em;cursor:pointer;">DONE</div><br><br><br><br>用户及密码，若清空则使用当前登录用户<br><br><textarea id="users" style="border-radius:5px;padding:1em;outline:none;width:90%;height:8em;">用户一\n用户一密码\n用户二\n用户二密码\n</textarea><br><br>帖子地址<br><br><textarea id="targets" style="width:90%;outline:none;height:8em;border-radius:5px;padding:1em;">帖子一地址\n帖子二地址</textarea></div></div>');
 
 // 将这些东西写入localStroage
 localStorage.setItem('targetPosts', JSON.stringify(targetPosts));
